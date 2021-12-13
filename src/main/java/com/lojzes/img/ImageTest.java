dfc1a616d7659f58793621782dcfa1c4b6b6cc70
liaoyuanyuan <lojzes@qq.com>
2021-12-13 18:28:03 +0800

    init

--- src/main/java/com/lojzes/img/ImageTest.java
+++ src/main/java/com/lojzes/img/ImageTest.java
@@ -10,6 +10,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageOutputStream;
+
 /**
  * 描述:
  * <p> // todo
@@ -21,9 +22,9 @@ public class ImageTest {
 
   public static void main(String[] args) throws IOException {
     //d1(30);
-   // d2();
-   // d3(30,"爱新觉罗张三丰");
-      d3(30,"刘明树爱心觉罗中国");
+    // d2();
+    // d3(30,"爱新觉罗张三丰");
+    d3(30, "刘明树爱");
 
 //    int fontSize = 30;
 //    int width = fontSize * 3 + 4;
@@ -54,7 +55,7 @@ public class ImageTest {
 
 
   public static void d1(int fontSize) throws IOException {
-   // int fontSize = 25;
+    // int fontSize = 25;
     // 字体左右 距 画板的距离
     int w = 13;
     int width = fontSize * 3 + w * 2;
@@ -66,35 +67,44 @@ public class ImageTest {
     // 设置背景色
     graphics.setColor(Color.white);
     // 画一个矩形
-    graphics.fillRect(0,0,width,height);
+    graphics.fillRect(0, 0, width, height);
     // 设置字体
     Font font = new Font("楷体", Font.BOLD, fontSize);
     graphics.setFont(font);
     // 设置字体颜色
     graphics.setColor(Color.black);
     // 写字
-    int y = fontSize + fontSize/2;
-    graphics.drawString("张三丰",w,y);
+    int y = fontSize + fontSize / 2;
+    graphics.drawString("张三丰", w, y);
     // 释放对象
     graphics.dispose();
     ImageIO.write(bufferedImage, "png", new File("d:\\test.png"));
   }
 
-  public static void d3(int fontSize,String name) throws IOException {
+  public static void d3(int fontSize, String name) throws IOException {
     // 字体长度
     int nameCharSize = name.toCharArray().length;
     // int fontSize = 25;
-    // 字体左右 距 画板的距离
+    // 字体左右 距画板的距离
     int w = 3;
-    if(nameCharSize == 2){
-      char[] chars = name.toCharArray();
-      name = chars[0] + " " + chars[1];
-      nameCharSize = 3;
+    // 适应字体右边的距离
+    int plus = 5;
+    if (nameCharSize == 4) {
+      plus = 7;
+    } else if (nameCharSize == 5) {
+      plus = 10;
+    } else if (nameCharSize == 6) {
+      plus = 12;
+    } else if (nameCharSize == 7) {
+      plus = 13;
+    } else if (nameCharSize == 8) {
+      plus = 16;
+    } else if (nameCharSize == 9) {
+      plus = 17;
     }
 
-    int width = fontSize * nameCharSize + w * 2 + 4 + 10;
-    int height = fontSize  + 10 ;
-
+    int width = fontSize * nameCharSize + w * 2 + plus + 3;
+    int height = fontSize + 10;
     // 创建BufferedImage 对象
     BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
     // 获取画笔
@@ -102,15 +112,15 @@ public class ImageTest {
     // 设置背景色
     graphics.setColor(Color.white);
     // 画一个矩形
-    graphics.fillRect(0,0,width,height);
+    graphics.fillRect(0, 0, width, height);
     // 设置字体
-    Font font = new Font("楷体", Font.BOLD, fontSize);
+    Font font = new Font("宋体", Font.BOLD, fontSize);
     graphics.setFont(font);
     // 设置字体颜色
     graphics.setColor(Color.black);
     // 写字
     int y = fontSize;
-    graphics.drawString(name,w,y);
+    graphics.drawString(name, w, y);
     // 释放对象
     graphics.dispose();
 
@@ -125,13 +135,15 @@ public class ImageTest {
     int borderedImageWidth = width + borderLeft * 2;
     int borderedImageHeight = height + borderTop * 2;
 
-    BufferedImage img = new BufferedImage(borderedImageWidth, borderedImageHeight, BufferedImage.TYPE_3BYTE_BGR);
+    BufferedImage img = new BufferedImage(borderedImageWidth, borderedImageHeight,
+        BufferedImage.TYPE_3BYTE_BGR);
     img.createGraphics();
     Graphics2D g = (Graphics2D) img.getGraphics();
     g.setColor(Color.black);
     g.fillRect(0, 0, borderedImageWidth, borderedImageHeight);
 
-    g.drawImage(source, borderLeft, borderTop , width + borderLeft , height + borderTop, 0, 0, width, height, Color.YELLOW, null);
+    g.drawImage(source, borderLeft, borderTop, width + borderLeft, height + borderTop, 0, 0, width,
+        height, Color.YELLOW, null);
 
     ImageIO.write(img, "png", file);
   }
